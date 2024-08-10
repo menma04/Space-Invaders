69f9b9ee403649ba4d21ea37eb960c95de20694f
Chetana Saikia <saikiachetana14@gmail.com>
2024-08-10 16:37:22 +0530

    Update main.cpp

--- Space-Invaders/main.cpp
+++ Space-Invaders/main.cpp
@@ -21,23 +21,39 @@ int main()
         // Clear the window
         window.clear(sf::Color::Black);
 
-        sf::CircleShape circle(50);
-        circle.setFillColor(sf::Color::Green);
-        circle.setPosition(0, 0);
+        //sf::CircleShape circle(50);
+        //circle.setFillColor(sf::Color::Green);
+        //circle.setPosition(0, 0);
 
-        sf::CircleShape traingle(50);
-        traingle.setPointCount(3);
-        traingle.setFillColor(sf::Color::Blue);
-        traingle.setPosition(400, 300);
+        //sf::CircleShape traingle(50);
+        //traingle.setPointCount(3);
+        //traingle.setFillColor(sf::Color::Blue);
+        //traingle.setPosition(400, 300);
 
-        sf::CircleShape square(50);
-        square.setPointCount(4);
-        square.setFillColor(sf::Color::Red);
-        square.setPosition(sf::Vector2f(600,300));
+        //sf::CircleShape square(50);
+        //square.setPointCount(4);
+        //square.setFillColor(sf::Color::Red);
+        //square.setPosition(sf::Vector2f(600,300));
 
-        window.draw(circle);
-        window.draw(traingle);
-        window.draw(square);
+        //window.draw(circle);
+        //window.draw(traingle);
+        //window.draw(square);
+        
+        sf::Texture outscal_texture;
+        outscal_texture.loadFromFile("assets/textures/outscal_logo.png");
+
+        sf::Sprite sprite;
+        sprite.setTexture(outscal_texture);
+        //sprite.setRotation(45);
+        sprite.setPosition(100, 300);
+        window.draw(sprite);
+
+        sf::Font font;
+        font.loadFromFile("assets/fonts/OpenSans.ttf");
+        sf::Text text("SFML is Awesome!", font, 50);
+        text.setFillColor(sf::Color::Red);
+        window.draw(text);
+
         // Display whatever you draw
         window.display();
     }
