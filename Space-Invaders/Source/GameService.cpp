1496d0097a46ea5e85f665114a312c707d3f4bb9
Chetana Saikia <saikiachetana14@gmail.com>
2024-08-12 09:17:05 +0530

    Creating the game
    Creating the game loop

---
+++ Space-Invaders/Source/GameService.cpp
@@ -0,0 +1,31 @@
+#include"../header/GameService.h"
+
+void GameService::destructor() {
+	//empty here
+}
+
+GameService::GameService() {}
+
+void GameService::ignite(){
+ //empty here
+}
+
+void GameService::initialize() {
+	//empty here
+}
+
+void GameService::isRunning() {
+	//empty here
+}
+
+void GameService::render() {
+	//empty here
+}
+
+void GameService::update() {
+	//empty here
+}
+
+GameService::~GameService() {
+	
+}
